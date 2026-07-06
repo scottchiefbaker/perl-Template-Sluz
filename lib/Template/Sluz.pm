3db154db17aa2768c11de5edf06b6fb6cdce8d24
Scott Baker <scott@perturb.org>
2026-07-06 14:44:29 -0700

    Bump to even/release version

--- lib/Template/Sluz.pm
+++ lib/Template/Sluz.pm
@@ -24,7 +24,7 @@ use autouse 'Carp' => qw(croak);
 
 use constant SLUZ_INLINE => 'INLINE_TEMPLATE';
 
-our $VERSION = 'v0.9.5';
+our $VERSION = 'v0.9.6';
 
 ################################################################################
 # Built-in Sluz functions that can be used in templates
